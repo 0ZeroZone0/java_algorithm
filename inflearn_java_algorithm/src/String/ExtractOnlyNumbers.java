c6a77a6187d73f749a587600f2a45df78f8708da
0ZeroZone <00zero0zone00@gmail.com>
2023-02-24 20:03:05 +0900

    [Add] ExtractOnlyNumbers commit

--- inflearn_java_algorithm/src/String/ExtractOnlyNumbers.java
+++ inflearn_java_algorithm/src/String/ExtractOnlyNumbers.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class ExtractOnlyNumbers {
 
 	public int solution(String s) {
-		int answer = 0;
+		String answer = "";
 		for(char x : s.toCharArray()) {
-			if(x >= 48 && x <= 57) {
-				answer = answer * 10 + (x-48);
+			if(Character.isDigit(x)) {
+				answer += x;
 			}
 		}
-		return answer;
+		return Integer.parseInt(answer);
 	}
 	
 	
